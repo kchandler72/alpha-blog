ec56fdb074121ebd38488c7b18730835af05f79b
Ken Chandler <kenatwork72@yahoo.com>
2017-10-06 20:42:32 +0000

    Added Articles route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 root 'pages#home'
 get 'about', to: 'pages#about'
 
+resources :articles
+
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
