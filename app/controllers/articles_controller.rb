71b687c277832f8783d5ff649d10cb65bd7bd710
Ken Chandler <kenatwork72@yahoo.com>
2017-10-08 15:55:05 +0000

    Added show, edit, new and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
     
+    def index
+      @articles = Article.all
+    end
+    
     def new
       @article = Article.new
+    end
+    
+    def edit
+      @article = Article.find(params[:id])
     end
     
     def create
@@ -16,6 +24,16 @@ class ArticlesController < ApplicationController
       
     end
     
+    def update
+     @article = Article.find(params[:id])
+     if @article.update(article_params)
+       flash[:notice] = "Article updated successfully!"
+       redirect_to article_path(@article)
+     else
+       render 'edit'
+     end
+    end
+    
     def show
       @article = Article.find(params[:id])
     end
@@ -25,4 +43,5 @@ class ArticlesController < ApplicationController
     def article_params
        params.require(:article).permit(:title, :description) 
     end
+    
 end
