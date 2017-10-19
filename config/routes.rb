e892a175c15b0cba6399f50f351482f742e76436
Ken Chandler <kenatwork72@yahoo.com>
2017-10-19 19:23:05 +0000

    Added user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ get 'about', to: 'pages#about'
 
 resources :articles
 
+get 'signup', to: 'users#new'
 
+resources :users, except: [:new]
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
