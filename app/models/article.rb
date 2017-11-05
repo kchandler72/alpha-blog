d31230acaf6b33dc2e7887a9d702b4663aba36e3
Ken Chandler <kenatwork72@yahoo.com>
2017-11-05 17:13:03 +0000

    increased max article size to 500 characters

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ActiveRecord::Base
     has_many :article_categories
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: {minimum: 3, maximum: 30}
-    validates :description, presence: true, length:{minimum: 10, maximum: 300}
+    validates :description, presence: true, length:{minimum: 10, maximum: 500}
     validates :user_id, presence: true
 end
